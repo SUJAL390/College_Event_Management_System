5e5b35d6f582e42322eb7c49f58ba41b4854a0a1
Sujal Maharjan <85659920+SUJAL390@users.noreply.github.com>
2025-05-30 08:57:45 +0545

    minor changes

--- backend/app/db/init_db.py
+++ backend/app/db/init_db.py
@@ -11,13 +11,13 @@ logger = logging.getLogger(__name__)
 
 # Create initial admin user
 def init_db(db: Session) -> None:
-    # Create tables if they don't exist
+    # Creates tables if  don't exist
     Base.metadata.create_all(bind=engine)
     
-    # Check if we already have users
+ 
     user = db.query(User).first()
     if not user:
-        # Create admin user
+        
         admin = User(
             username=settings.FIRST_ADMIN_USERNAME,
             email=settings.FIRST_ADMIN_EMAIL,
