b7519e9a7c12cb07b498d9107baed1398a854109
Sujal Maharjan <85659920+SUJAL390@users.noreply.github.com>
2025-06-24 22:19:39 +0545

    Update setup_db.bat

--- backend/setup_db.bat
+++ backend/setup_db.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Creating PostgreSQL database...
-set PGPASSWORD=CLFA63827S
+set PGPASSWORD="your_pg_password"
 
 psql -U postgres -c "CREATE DATABASE college_events;"
 echo Database created successfully!
